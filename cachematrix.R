63bc16a006516c7a129d550f171c6624334c3cec
Danail Hristov <danail.hristov@kpn.com>
2017-12-22 19:19:30 +0100

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+rm(a)
+mm=matrix(c(2:5),2,2)
+m=matrix(c(1:4),2,2)
+## This function creates a special "myMatrix" object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(myMatrix = matrix()) {
+  ## The function creates a list of 4 functions,
+  ## setMatrix -> will set the initial value of myMatrix and its inverse if myMatrix is new
+  ## getMatrix -> will print myMatrix
+  ## setInvMatrix -> will set invMatrix 
+  ## getInvMatrix -> will print invMatrix  
+  invMatrix <- NULL
+  setMatrix <- function(y) {
+    if(identical(y,myMatrix)){
+      message("Matrices are the same,\n Use cacheSOlve() or getInvMatrix() to get its inverse:\n")
+      
+    }else{
+      myMatrix <<- y
+      invMatrix <<- NULL
+    }
+    
+  }
+  getMatrix <- function() myMatrix
+  setInvMatrix <- function(solve) invMatrix <<- solve
+  getInvMatrix <- function() invMatrix
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(myMatrix, ...) {
+  ## In -> myMatrix
+  ## Out -> invMatrix
+  ## the function will calculate the inverse of myMatrix 
+  ## if myMatrix is new and will cache it for later use if myMatrix does not change
+  invMatrix <- myMatrix$getInvMatrix()
+  if(!is.null(invMatrix)) {
+    message("Inverse has been calculated -> getting cached data")
+    return(invMatrix)
+  }else {
+    data <- myMatrix$getMatrix()
+    invMatrix <- solve(data, ...)
+    myMatrix$setInvMatrix(invMatrix)
+    invMatrix
+  }
+    
+  
 }
-lallalalalalalallal
+
+
